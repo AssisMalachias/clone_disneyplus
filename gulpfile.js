be20b176d992e3452d0b2a2c2cc78a38d21a251a
Francisco Assis Malachias de Almeida <127680274+AssisMalachias@users.noreply.github.com>
2025-07-07 12:42:43 -0300

    criando  o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function style() {
     return gulp.src('./src/style/*.scss')
-    .pipe(sass({ outputStyle: 'campressed' }))
-    .pipe(gulp.dest('./dist/style'));
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest('./dist/css/'));
 }
 
-exports.default = style;
+function optimizeImagens() {
+    return gulp.src('./src/image/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/image/'));
+}
+
+
+exports.default = gulp.parallel(style, optimizeImagens);
 exports.watch = function() {
     gulp.watch('./src/style/*.scss', gulp.parallel(style))
 }
